917730f307170aec9fd44cc0added85d52602997
Romario Melo <romariomelo360@gmail.com>
2025-06-24 01:07:04 -0300

    create construct and init od memori, with tests of print values

--- problems/forca-minimal/src/forca-minimal.c
+++ problems/forca-minimal/src/forca-minimal.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <time.h>
+#include <stdint.h>
+#include <stddef.h>
 
+#define MAX_BYTES_MEMORI 1000
+const char *data = "[Technology|Computer-Programming-Algorithm-Internet-Software[Nature|Forest-River-Mountain-Animal-Plant[Colors|Red-Blue-Green-Yellow-Purple[Food|Pizza-Rice-Apple-Chocolate-Salad[Human Body|Heart-Lung-Brain-Hand-Eye[Transport|Car-Airplane-Bicycle-Train-Boat]";
+const size_t data_size = 256;
+
+void construct_memori(uint8_t **memori)
+{
+    *memori = malloc(sizeof(char) * MAX_BYTES_MEMORI);
+
+    if(*memori == NULL)
+    {
+        perror("malloc failed in memori");
+        exit(EXIT_FAILURE);
+    }
+
+    (*memori)[MAX_BYTES_MEMORI] = '\0';
+}
+
+void init_memori(uint8_t **memori)
+{
+    for(size_t i=0; i < data_size; i++)
+    {
+        (*memori)[i] = data[i];
+    }
+}
 
 int main()
 {
-    printf("Hello Forca Minimal!");
+    
+    uint8_t *memori;
+    construct_memori(&memori);
+    init_memori(&memori);
 
+    for(size_t i=0; i < data_size; i++)
+    {
+        printf("%c", (memori)[i]);
+    }
+
+    free(memori);
     return 0;
 }
