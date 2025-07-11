9b8cc8ab0fe718c5df11c929db4e15c4161d9adf
Romario Melo <romariomelo360@gmail.com>
2025-07-11 15:20:47 -0300

    binomial heap merge tests

--- problems/data-structures/src/binomial-heap.h
+++ problems/data-structures/src/binomial-heap.h
@@ -1,5 +1,8 @@
 typedef struct heap Heap;
 Heap *ConstructHeap(int value);
+Heap *merge(Heap *h1, Heap *h2);
+Heap *combine_heaps(Heap *h1, Heap *h2);
+void print_heap(Heap *heap);
 
 
 struct heap
@@ -8,7 +11,7 @@ struct heap
     int degree;
 
     struct heap *parent;
-    struct heap *next;
+    struct heap *sibling;
     struct heap *child;
 
 };
@@ -20,10 +23,99 @@ Heap *ConstructHeap(int value)
     heap->value = value;
     heap->degree = 0;
     heap->parent = NULL;
-    heap->next = NULL;
+    heap->sibling = NULL;
     heap->child = NULL;
 
     return heap;
 }
 
+void print_heap(Heap *heap)
+{
+    if(heap != NULL)
+        printf("(%i) ",heap->value);
 
+    if(heap->parent != NULL)
+        printf("parent=%i ",heap->parent->value);
+
+    if(heap->sibling  != NULL)
+        printf("sibling=%i ",heap->sibling ->value);
+
+    if(heap->child != NULL)
+        printf("child=%i ",heap->child->value);
+    
+    printf("\n");
+
+    if(heap->child != NULL)
+        print_heap(heap->child);
+
+    if(heap->sibling  != NULL)
+        print_heap(heap->sibling );
+}
+
+Heap *merge(Heap *h1, Heap *h2)
+{
+    Heap *back, *x, *next;
+    Heap *h = combine_heaps(h1, h2);
+
+    x = h;
+
+    if(h == NULL) return h;
+
+    next = x->sibling;
+
+    while(next != NULL)
+    {
+        if((x->degree != next->degree) || ((next->sibling != NULL) && (next->sibling->degree == x->degree)))
+        {
+            back = x;
+            x = next;
+        }
+    }
+
+
+    return h;
+}
+
+Heap *combine_heaps(Heap *h1, Heap *h2)
+{
+    Heap *h, *a, *b, *y, *z;
+    y = h1;
+    z = h2;
+
+    if(y != NULL)
+    {
+        if((z != NULL) && (y->degree <= z->degree))
+            h = y;
+        else if((y != NULL) && (y->degree > h2->degree))
+            h = h2;
+    }
+    else
+    {
+        h = h2;
+    }
+
+    while((y != NULL) && (z != NULL))
+    {
+        if(y->degree <z->degree)
+        {
+            y = z->sibling;
+        }
+        else
+        {
+            if(y->degree == z->degree)
+            {
+                a = y->sibling;
+                y->sibling = z;
+                y = a;
+            }
+            else
+            {
+                b = z->sibling;
+                z->sibling = y;
+                z = b;
+            }
+        }
+    }
+
+    return h;
+}
--- problems/data-structures/tests/binomial-heap-test.c
+++ problems/data-structures/tests/binomial-heap-test.c
@@ -6,10 +6,32 @@
 
 int main()
 {
-    Heap *h = ConstructHeap(10);
+    
+    // Raiz principal do B4
+    Heap *h = ConstructHeap(1);
 
-    printf("value=%i",h->value);
+    // Definindo os 4 filhos da raiz (raízes das sub-árvores B3, B2, B1, B0)
+    h->child = ConstructHeap(2);
+    h->child->sibling = ConstructHeap(3);
+    h->child->sibling->sibling = ConstructHeap(4);
+    h->child->sibling->sibling->sibling = ConstructHeap(5);
 
+    // Agora definindo os filhos do nó '2' (que é a raiz da sub-árvore B3)
+    h->child->child = ConstructHeap(6);
+    h->child->child->sibling = ConstructHeap(7);
+    h->child->child->sibling->sibling = ConstructHeap(8);
+
+    // E os filhos do nó '3' (que é a raiz da sub-árvore B2)
+    h->child->sibling->child = ConstructHeap(9);
+    h->child->sibling->child->sibling = ConstructHeap(10);
+
+    // E assim por diante...
+    print_heap(h);
+    
+
+    Heap *h2 = ConstructHeap(65);
+
+    merge(h, h2);
     
     return 0;
 }
